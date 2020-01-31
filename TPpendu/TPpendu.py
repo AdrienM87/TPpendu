3d2a08466a723dcad5dd5fc3767e177d23e48a53
ADRIEN MARINI <adrien.marini@viacesi.fr>
2020-01-31 20:02:56 +0100

    chargement de la liste de mots + test

--- TPpendu/TPpendu.py
+++ TPpendu/TPpendu.py
@@ -1,1 +1,15 @@
-#liste de mots
+# main test
+if __name__ == "__main__":
+
+    #words list charging
+    fileWords = open("C:\_Projets VS19\TPpendu\TPpendu\english-usa-words.txt", "r")
+
+    strWords = fileWords.read()
+    listWords = strWords.split()
+
+    for w in listWords:
+        print(w)
+
+    fileWords.close()
+
+    input("")
