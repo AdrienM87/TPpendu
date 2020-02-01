9cfef1f242f4fa2b83e9f96db3d7644e25081e1d
ADRIEN MARINI <adrien.marini@viacesi.fr>
2020-02-01 12:57:56 +0100

    décomptage des essais (vies) et proposition de nouvelle partie en cas de défaite

--- TPpendu/TPpendu.py
+++ TPpendu/TPpendu.py
@@ -17,44 +17,73 @@ if __name__ == "__main__":
     print("You have 8 chances to find it by trying letters one by one.")
     print("")
 
-    continueChoice = 1
-    while continueChoice == 1:   #bouclage sur les participations au jeu
+
+    isNewGame = 1     #bouclage sur les parties
+    while isNewGame == 1:
+
+        nbLives = 8  #reset
 
         #choix random d'un mot dans la liste
         nbMax = len(listWords)
         wordChoosed = listWords[random.randint(0, nbMax)]
 
-        nbLives = 8
         print("Fine! Good luck :)")
         print("")
 
-        validTry = 0
-        while validTry != 1:    #bouclage sur la validité de la saisie
 
-            letterTried = input("Your try ('0' to quit) : ")
-            inputLength = len(letterTried)
+        isContinued = 1
+        while isContinued == 1 and nbLives > 0:   #bouclage sur les tentatives
 
-            if inputLength == 0:
-                validTry = 0
+            isValidTry = 0
+            while isValidTry != 1:    #bouclage sur la validité de la saisie
 
-            elif inputLength > 1:
-                validTry = 0
-                print("Invalid try. Only one by one.")
+                letterTried = input("Your try ('0' to quit) : ")
+                inputLength = len(letterTried)
 
-            elif letterTried == "0":
-               validTry = 1
-               continueChoice = 0
-            else:
-                validTry = re.search("[a-zA-Z]", letterTried)
+                if inputLength == 0:
+                    isValidTry = 0
 
-                if not validTry:
+                elif inputLength > 1:
+                    isValidTry = 0
+                    print("Invalid try. Only one by one.")
+
+                elif letterTried == "0":
+                    isValidTry = 1
+                    isContinued = 0
+                
+                elif not re.search("[a-zA-Z]", letterTried):
+                    isValidTry = 0
                     print("Invalid try. Only letters are expected.")
-                else:
-                    #cas d'entrée de lettre valide : application de l'essai
+
+
+                else:   #cas d'entrée de lettre valide : application de l'essai
+                    isValidTry = 1
+
                     if letterTried in wordChoosed:
                         print("Well done!")
                     else:
                         print("What a shame!")
+                        nbLives -= 1
+        
+        if nbLives == 0:
+            print("Looser!")
+            print("")
+            
+        #proposition de nouvelle partie
+        isValidAnswer = 0
+        while isValidAnswer != 1:    #bouclage sur la validité de la saisie
+            print("Wanna try a new game?")
+            answer = input("Yes or No ? (y/n) : ")
+                        
+            if answer == "y" or answer == "Y":
+                isValidAnswer = 1
+                isNewGame = 1
+                print("")
+            elif answer == "n" or answer == "N":
+                isValidAnswer = 1
+                isNewGame = 0
+            else:
+                isValidAnswer = 0
 
 
     #test
